ad457b2e02c64d2e6122464221562f166bbf652a
mehakgp <156295236+mehakgp@users.noreply.github.com>
2024-01-13 01:12:07 +0530

    Update javascript.js

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,3 +1,19 @@
+var flag=true;
+function validateField(fieldName) {
+    var inputElement = document.getElementById(fieldName);
+    var errorElement = document.getElementById(fieldName + 'Error');
+
+    inputElement.classList.remove('error-border');
+    errorElement.textContent = '';
+
+    if (userDetails[fieldName].trim() === '') {
+        errorElement.textContent = fieldName + ' is required.';
+        inputElement.classList.add('error-border');
+        flag=false;
+    }
+}
+
+
 function submitForm() {
     var userDetails = {};
     var elements = document.querySelectorAll('.details');
@@ -7,5 +23,39 @@ function submitForm() {
         var value = element.value;
         userDetails[id] = value;
     });
+    for (var key in userDetails) {
+        if (userDetails.hasOwnProperty(key))
+         {
+            if (key === 'firstName')
+                validateField('firstName');
+            if (key === 'address1')
+                validateField('address1');
+            if (key === 'code')
+                validateField('code');
+            if (key === 'email')
+                validateField('email');
+            if (key === 'dob')
+                validateField('dob');
+            if (key === 'phoneno')
+                validateField('phoneno');
+            if (key === 'age')
+                validateField('age');
+            if (key === '10thmarks')
+                validateField('10thmarks');
+            if (key === '12thmarks')
+                validateField('12thmarks');
+            if (key === 'cgpa')
+                validateField('cgpa');
+                if (key === 'photo')
+                validateField('resume');            
+                if(flag==false)
+                return;
+        
+            
+            
 
+        }
+    }
+   
+    
 }
