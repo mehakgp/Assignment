3496687ba97ae2cb4f68fe34f08ffcbad8946600
mehakgupta916 <mehak.g@mindfiresolutions.com>
2024-01-30 14:39:00 +0530

    added layers

---
+++ CRUD_ENTITY_FRAMEWORK.BusinessLayer/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CRUD_ENTITY_FRAMEWORK.UtilityLayer.Class1;
+using static CRUD_ENTITY_FRAMEWORK.DataAccessLayer.Class1;
+using System.IO;
+using CRUD_ENTITY_FRAMEWORK.DataAccessLayer;
+
+namespace CRUD_ENTITY_FRAMEWORK.BusinessLayer
+{
+    public class Class1
+    {
+  
+        public static void DisplayCrudOptions()
+        {
+            DataAccessLayer.Class1.DisplayCrudOptions();
+
+        }
+       public static void LogException(Exception ex)
+        {
+           DataAccessLayer.Class1.LogException(ex);
+        }
+
+        public static void CreateOperation()
+        {
+            DataAccessLayer.Class1.CreateOperation();
+        }
+       public static void ReadOperation()
+        {
+           DataAccessLayer.Class1.ReadOperation();
+        }
+        public static void UpdateOperation()
+        {
+            DataAccessLayer.Class1.UpdateOperation();
+        }
+        public static void DeleteOperation()
+        {
+           DataAccessLayer.Class1.DeleteOperation();
+        }
+        public enum CrudOperation
+        {
+            Create = 1,
+            Read,
+            Update,
+            Delete,
+            Exit
+        }
+
+    }
+}
